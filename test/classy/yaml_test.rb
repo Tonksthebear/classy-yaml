f0fecf553c5d8d87c1f9b92804c093e3274aa237
Tonksthebear <jasonconig@me.com>
2021-11-11 18:03:58 -0800

    Add introductory tests to cover basic functions

--- test/classy/yaml_test.rb
+++ test/classy/yaml_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
+include Classy::Yaml::Helpers
 
 class Classy::YamlTest < ActiveSupport::TestCase
-  test "it has a version number" do
-    assert Classy::Yaml::VERSION
+  test "can fetch single utility class" do
+    assert_equal "single-class", yass(:single)
+  end
+
+  test "can fetch nested utility classes" do
+    assert_equal "nested-no-base-class", yass(nested_no_base: :nested)
+  end
+
+  test "can fetch multiple nested utility classes" do
+    assert_equal "nested-no-base-class nested2-class", yass(nested_no_base: [:nested, :nested2])
+  end
+
+  test "includes base if found to nested" do
+    assert_equal "nested-base-class nested-class", yass(nested_base: :nested)
+  end
+
+  test "can fetch multiple at same time" do
+    assert_equal "single-class nested-no-base-class", yass(:single, nested_no_base: :nested)
   end
 end
--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -1,6 +1,14 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+require "active_model/railtie" 
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "active_job/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

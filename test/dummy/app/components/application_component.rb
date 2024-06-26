69debd8e54b43aae4404434a61f9cf82fa28419c
Tonksthebear <jasonconig@me.com>
2024-06-26 16:17:24 -0700

    Add support for engines to add utility classes that are processed first for easy overriding

--- test/dummy/app/components/application_component.rb
+++ test/dummy/app/components/application_component.rb
@@ -1,11 +1,21 @@
 class ApplicationComponent < ViewComponent::Base
   include Classy::Yaml::ComponentHelpers
+  attr_reader :class_count
+
   def initialize(classy:, inherited: false)
     @classy = classy
     @inherited = inherited
   end
 
+  def inherited_classes
+    @inherited_classes ||= yass(@classy)
+  end
+
+  def class_count
+    @class_count ||= @inherited ? inherited_classes&.split(' ')&.length : classes&.split(' ').length
+  end
+
   def inherited_call
-    yass(@classy)
+    inherited_classes
   end
 end
--- test/dummy/app/components/test_component.rb
+++ test/dummy/app/components/test_component.rb
@@ -1,11 +1,24 @@
 class TestComponent < ApplicationComponent
+  def before_render
+    @classes = @inherited ? inherited_call : yass(@classy)
+    @class_count = @classes.split(' ').length
+  end
+
+  def classes
+    @classes ||= @inherited ? inherited_classes : yass(@classy)
+  end
+
   def call
-    @inherited ? inherited_call : yass(@classy)
+    @inherited ? inherited_call : classes
   end
 
   class NestedComponent < TestComponent
+    def classes
+      @classes ||= @inherited ? inherited_classes : yass(@classy)
+    end
+
     def call
-      @inherited ? inherited_call : yass(@classy)
+      @inherited ? inherited_call : classes
     end
   end
 end
